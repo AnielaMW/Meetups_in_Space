73cbc516cd4e9916a1b9f3dfe5841ddf1c890a58
Aniela M Wolkonowski <anielamw@gmail.com>
2017-01-07 01:12:14 -0500

    test first drafts

--- spec/features/2_user_views_meetup_list_spec.rb
+++ spec/features/2_user_views_meetup_list_spec.rb
@@ -9,5 +9,24 @@ feature "user views list of meetups" do
   # * On the meetups index page, I should see the name of each meetup.
   # * Meetups should be listed alphabetically.
 
-  pending "successfully view alphabetized list of meetups" 
+  pending "successfully view alphabetized list of meetups" do
+    tap_dance = Meetup.create!({
+      name: "Tap Dance Recital",
+      description: "Tango Cafe's annual Tap Dance Recital will be (date), (time), (tickets).",
+      location: "Tango Cafe",
+      creator: "me"
+      })
+    theatre = Meetup.create!({
+      name: "RBTO Amature Auditions",
+      description: 'Rochester Broadway Theatre Organization is holding auditions for the spring show, "Rent".',
+      location: "Auditorium Theatre",
+      creator: "you"
+      })
+
+    visit "/meetups"
+
+    expect(page).to have_content("RBTO Amature Auditions")
+    expect(page).to have_content("Tap Dance Recital")
+    # Test for alphabetical order????????????????????
+  end
 end
--- spec/features/3_user_views_meetup_details_spec.rb
+++ spec/features/3_user_views_meetup_details_spec.rb
@@ -9,5 +9,25 @@ feature "user views details of a meetup" do
   # * On the index page, the name of each meetup should be a link to the meetup's show page.
   # * On the show page, I should see the name, description,  location, and the creator of the meetup.
 
-  pending "successfully view details of a meetup"
+  pending "successfully view details of a meetup" do
+    tap_dance = Meetup.create!({
+      name: "Tap Dance Recital",
+      description: "Tango Cafe's annual Tap Dance Recital will be (date), (time), (tickets).",
+      location: "Tango Cafe",
+      user_id: 1
+      })
+    theatre = Meetup.create!({
+      name: "RBTO Amature Auditions",
+      description: 'Rochester Broadway Theatre Organization is holding auditions for the spring show, "Rent".',
+      location: "Auditorium Theatre",
+      user_id: 2
+      })
+
+    visit "/meetups"
+    click_link "Tap Dance Recital"
+
+    expect(page).not_to have_content("RBTO Amature Auditions")
+    expect(page).to have_content("Tap Dance Recital")
+    expect(page).to have_content("Tango Cafe")
+  end
 end
